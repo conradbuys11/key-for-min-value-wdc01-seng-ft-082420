1b19e690fe158e98c2bf95e83b6dd8b214fa5d00
Conrad Buys <conradbuys11@gmail.com>
2020-08-09 14:29:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,2 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+
+def key_for_min_value(hash)
+  smallest_key = nil
+  smallest_value = nil
+  
+  hash.each do |key, value|
+    if smallest_value == nil || 
+  end
+end
